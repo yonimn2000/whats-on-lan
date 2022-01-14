227bca0720bb473564aca0dd1aa2ef42635f8cf1
Yonatan Mankovich <yonimn2000@gmail.com>
2022-01-14 10:38:19 -0500

    Add hostname helper.

--- WhatsOnLan.Console/Program.cs
+++ WhatsOnLan.Console/Program.cs
@@ -8,9 +8,10 @@ List<Task> tasks = new List<Task>();
 foreach (IPAddress ip in IpAddressHelpers.GetAllHostAddresses(IPAddress.Parse("192.168.1.60"), IPAddress.Parse("255.255.255.0")))
     tasks.Add(Task.Run(() =>
     {
-        bool resolves = ArpHelpers.Resolves(ip);
-        bool pings = PingHelpers.PingIpAddress(ip);
-        Console.WriteLine(ip + "\t" + (resolves ? "R" : "-") +(pings ? "P" : "-"));
+        bool resolves = ArpHelpers.Resolves(ip) || ArpHelpers.Resolves(ip) || ArpHelpers.Resolves(ip);
+        bool pings = PingHelpers.PingIpAddress(ip) || PingHelpers.PingIpAddress(ip) || PingHelpers.PingIpAddress(ip);
+        if (resolves || pings)
+            Console.WriteLine(ip + "\t" + (resolves ? "R" : "-") + (pings ? "P" : "-") + " " + HostnameHelpers.GetHostname(ip));
     }));
 
 Task.WaitAll(tasks.ToArray());
