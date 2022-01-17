af8e5b427773abbb34d948f95be6db8a9024ed0e
Yonatan Mankovich <yonimn2000@gmail.com>
2022-01-17 18:20:23 -0500

    Update Net Interface Helpers.

--- WhatsOnLan.Core/Hardware/NetworkInterfaceHelpers.cs
+++ WhatsOnLan.Core/Hardware/NetworkInterfaceHelpers.cs
@@ -2,18 +2,38 @@
 
 namespace WhatsOnLan.Core.Hardware
 {
+    /// <summary>
+    /// Provides methods for getting the network interfaces of the current machine.
+    /// </summary>
     public class NetworkInterfaceHelpers
     {
-        public static IEnumerable<NetworkInterface> GetAllActiveInterfaces()
+        /// <summary>
+        /// Gets all distinct active non-loopback interfaces as an 
+        /// <see cref="IEnumerable{T}"/> of <see cref="PcapNetworkInterface"/> objects
+        /// distinguished by their network addresses.
+        /// </summary>
+        public static IEnumerable<PcapNetworkInterface> GetAllDistinctPcapNetworkInterfaces()
+        {
+            return GetAllPcapNetworkInterfaces().DistinctBy(i => i.Network);
+        }
+
+        /// <summary>
+        /// Gets all active non-loopback interfaces as an 
+        /// <see cref="IEnumerable{T}"/> of <see cref="PcapNetworkInterface"/> objects.
+        /// </summary>
+        public static IEnumerable<PcapNetworkInterface> GetAllPcapNetworkInterfaces()
+        {
+            return GetAllActiveInterfaces().Select(i => new PcapNetworkInterface(i));
+        }
+
+        /// <summary>
+        /// Gets all active non-loopback interfaces.
+        /// </summary>
+        private static IEnumerable<NetworkInterface> GetAllActiveInterfaces()
         {
             return NetworkInterface.GetAllNetworkInterfaces()
                 .Where(nic => nic.OperationalStatus == OperationalStatus.Up
                     && nic.NetworkInterfaceType != NetworkInterfaceType.Loopback);
-        }
-
-        public static IEnumerable<PcapNetworkInterface> GetAllPcapNetworkInterfaces()
-        {
-            return GetAllActiveInterfaces().Select(i => new PcapNetworkInterface(i));
         }
     }
 }
